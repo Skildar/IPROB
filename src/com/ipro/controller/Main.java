646f23a0131416341d440c9cf8f4f279a791af56
Husson Olivier <oca.husson@gmail.com>
2018-06-25 09:07:22 +0200

    IPROB update comments to better reflect how the app works

--- src/com/ipro/controller/Main.java
+++ src/com/ipro/controller/Main.java
@@ -14,20 +14,6 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        /**
-         * Calls a function that starts the reading process.
-         * It reads each .csv file and creates an object for every line found inside them (except header).
-         *
-         * Those objects are them stocked inside multiple ArrayList<>.
-         * Which are returned to this Main class.
-         *
-         * The goal here is to have every information at disposal using the model classes.
-         * Then we must display everything on the Graphic User Interface.
-         *
-         * Inputs, select list and buttons can be used to filter the information.
-         * For example, when a specific store is selected, we must show only articles from that specific store!
-         * As so, we need to have dedicated display variables that only contain this filtered information!
-         */
         try {
             print();
         } catch (ParseException e) {
@@ -39,17 +25,12 @@ public class Main {
 
 
     /**
-     * Based on the data objects and the filters, render a new list
-     * Store --> id or name
-     * Restock --> storeId
-     * Order --> storeId
+     * Using model classes, we create a list of objects relative to each type of data at disposal in respective .csv files.
+     * Then print the content of each created object in the console.
      * @throws FileNotFoundException
      * @throws ParseException
      */
     public static void print() throws FileNotFoundException, ParseException {
-        /**
-         * TODO use the graphic interface to display every information
-         */
         System.out.println("-----  ----- Articles -----  -----");
         for (Article article : Article.findAllArticle()) {
             System.out.println(article.getName());
